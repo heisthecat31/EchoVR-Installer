de3bdeac9a2ea0674b6fd96b38962d456b10ead5
he_is_the_cat <125207670+heisthecat31@users.noreply.github.com>
2026-02-06 23:19:10 +0000

    Update SDK versions and add packaging options
    Updated minSdk to 26 and changed Java compatibility to version 1.8. Added packaging options to exclude META-INF resources.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,34 +8,36 @@ android {
 
     defaultConfig {
         applicationId = "com.echovr.installer"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    packaging {
+        resources.excludes.add("META-INF/*") // Prevents signing conflicts
     }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Tools for APK Editing
+    implementation("com.github.iyxan23:zipalign-java:1.2.2")
+    implementation("com.github.MuntashirAkon:apksig-android:4.4.0")
 }
